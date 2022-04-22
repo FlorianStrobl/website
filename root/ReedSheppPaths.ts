180ca038a69df7106c9440d98c9962bb35534638
Florian Strobl <FlorianStrobl@outlook.de>
2022-04-22 15:32:43 +0200

    1

--- root/ReedSheppPaths.ts
+++ root/ReedSheppPaths.ts
@@ -90,9 +90,9 @@ function getRSR(
    * we have to wrap this around 180 degrees
    * we do the same thing for C/D to A/B
    * then we get the absolute difference between these two values to get centralAngle
+   *
+   * orientation: (1, 0)=0deg; (0, 1)=90deg; (-1, 0)=180deg; (0, -1)=270deg
    */
-
-  // TODO, fix division by 0 and negatives in the sqrts
 
   // #region get circles
   // the right cirlces of start car and end car
@@ -104,11 +104,9 @@ function getRSR(
   // distance between point A and B (circle1 and circle2)
   const AB: number = Math.sqrt((A.y - B.y) ** 2 + (A.x - B.x) ** 2);
   const CD: number = AB; // distance CD is the same as the one from AB
-  //const slopeAB: number = (A.y - B.y) / (A.x - B.x);
   // #endregion
 
   // #region get simple (outer) angles
-  // (1, 0)=0deg; (0, 1)=90deg; (-1, 0)=180deg; (0, -1)=270deg
   // the angle the car has to the circle if you trace around the circumference
   const startCarToAAngle: rad = correctRad(
     Math.atan2(car1.pos.y - A.y, car1.pos.x - A.x)
@@ -120,70 +118,39 @@ function getRSR(
   const cOrDAngle: rad = correctRad(
     Math.atan2(B.y - A.y, B.x - A.x) + Math.PI / 2
   );
-
-  // #region new TODO
-  // f(x) = invSlope * x + n, where we get n from the Point A
-  // const invSlope: number = -1 / slopeAB;
-  // console.log('inv slope', invSlope, slopeAB, A, B, r);
-  // // f(A.x) = A.y = invSlope * A.x + n => n = A.y - invSlope * A.x
-  // const n: number = A.y - invSlope * A.x;
-  // let _x1: number =
-  //   (invSlope * n + Math.sqrt(r * r * (invSlope ** 2 + 1) - n * n)) /
-  //   (invSlope ** 2 + 1);
-  // let _x2: number =
-  //   (invSlope * n - Math.sqrt(r * r * (invSlope ** 2 + 1) - n * n)) /
-  //   (invSlope ** 2 + 1);
-  // const C: pos = {
-  //   x: _x1,
-  //   y: invSlope * _x1 + n
-  // };
-  // const mirrorC: pos = {
-  //   x: _x2,
-  //   y: invSlope * _x2 + n
-  // };
-  // const cAngle: number = correctRad(Math.atan2(C.y - A.y, C.x - A.x));
-  // const mirrorCAngle: number = correctRad(
-  //   Math.atan2(mirrorC.y - A.y, mirrorC.x - A.x)
-  // );
-  // console.log(cAngle);
-  // #endregion
-  // #endregion
+  // its mirror NOT C/D Prime'
+  const cOrDAngle2: rad = correctRad(
+    Math.atan2(B.y - A.y, B.x - A.x) - Math.PI / 2
+  );
 
   // #region get inner angles
-  let innerAngleStartC: rad = 0;
-  if (startCarToAAngle > cOrDAngle)
-    innerAngleStartC = Math.PI * 2 + (cOrDAngle - startCarToAAngle);
-  else innerAngleStartC = cOrDAngle - startCarToAAngle;
-  // same for the other side
-  let innerAngleDEnd: rad = endCarToBAngle - cOrDAngle;
-  if (endCarToBAngle > cOrDAngle)
-    innerAngleDEnd = Math.PI * 2 + (cOrDAngle - endCarToBAngle);
-  else innerAngleDEnd = cOrDAngle - endCarToBAngle;
-  // #endregion
+  const innerAngleStartC: rad = Math.abs(cOrDAngle - startCarToAAngle);
+  const innerAngleStartCPrime: rad = Math.PI * 2 - innerAngleStartC;
+  //if (startCarToAAngle > cOrDAngle) innerAngleStartC = Math.PI*2 - ...
+  //else innerAngleStartC = ...
 
-  // console.log(
-  //   'start car to A angle: ',
-  //   radToDeg(startCarToAAngle),
-  //   'C angle:',
-  //   radToDeg(cOrDAngle),
-  //   'inner angle start:',
-  //   radToDeg(innerAngleStartC),
-  //   'inner angle end:',
-  //   radToDeg(innerAngleDEnd)
-  // );
+  // same for the other side
+  const innerAngleDEnd: rad = Math.abs(cOrDAngle - endCarToBAngle);
+  const innerAngleDPrimeEnd = Math.PI * 2 - innerAngleDEnd;
+  // #endregion
 
   // Length=radius*innerAngle
   const lengthArc1: number = r * correctRad(innerAngleStartC); // some code
   const lengthArc2: number = r * correctRad(innerAngleDEnd);
 
-  // console.log(
-  //   'length linear: ',
-  //   CD,
-  //   'length arc 1: ',
-  //   lengthArc1,
-  //   'length arc 2: ',
-  //   lengthArc2
-  // );
+  const lengthArcPrime1: number = r * correctRad(innerAngleStartCPrime); // some code
+  const lengthArcPrime2: number = r * correctRad(innerAngleDPrimeEnd);
+
+  // newX = oldX + v * cos(theta)
+
+  // TODO check if it is forwards or backwards
+  console.log(
+    'path lengths: ',
+    Math.round(lengthArc1 + lengthArc2 + CD),
+    Math.round(lengthArcPrime1 + lengthArc2 + CD),
+    Math.round(lengthArcPrime2 + lengthArc1 + CD),
+    Math.round(lengthArcPrime1 + lengthArcPrime2 + CD)
+  );
 
   return {
     startCarToAAngle: startCarToAAngle,
@@ -191,63 +158,16 @@ function getRSR(
     cOrDAngle: cOrDAngle,
     innerAngleStartC: innerAngleStartC,
     innerAngleDEnd: innerAngleDEnd,
+    innerAngleStartCPrime: innerAngleStartCPrime,
+    innerAngleDPrimeEnd: innerAngleDPrimeEnd,
     lengthArc1: lengthArc1,
     lengthArc2: lengthArc2,
+    lengthArcPrime1: lengthArcPrime1,
+    lengthArcPrime2: lengthArcPrime2,
+    cOrDAngle2: cOrDAngle2,
     lengthTotalDistance: lengthArc1 + CD + lengthArc2
   } as unknown as number;
   //return lengthArc1 + CD + lengthArc2;
-
-  // // orthogonal slope to AB, along this is C and mirrorC/D and mirrorD
-  // let invAB: number =
-  //   -1 / ((circle2.y - circle1.y) ** 2 / (circle2.x - circle1.x) ** 2);
-  // // car start position to the circle1
-  // let angleCar1ToCircle1: number =
-  //   (car1.pos.y - circle1.y) ** 2 / (car1.pos.x - circle1.x) ** 2;
-  // // car end position to the circle2
-  // let angleCar2ToCircle2: number =
-  //   (car2.pos.y - circle2.y) ** 2 / (car2.pos.x - circle2.x) ** 2;
-
-  // // difference in angle between car and point C/D, (the inner angle)
-  // let arcLength1: number = arcLength(
-  //   circle1,
-  //   circle2,
-  //   invAB,
-  //   angleCar1ToCircle1,
-  //   r
-  // ); // car to C forward vs car to C backwards, with in mind 2*r*pi (half)
-  // let arcLength2: number = arcLength(
-  //   circle1,
-  //   circle2,
-  //   invAB,
-  //   angleCar2ToCircle2,
-  //   r
-  // ); // D to car forward vs D to car backwards, with in mind 2*r*pi (half)
-
-  // return arcLength1 + CD + arcLength2;
-
-  // function arcLength(
-  //   circ1: pos,
-  //   circ2: pos,
-  //   slope1: number,
-  //   slope2: number,
-  //   r: number
-  // ): number {
-  //   // angle for C or D to A or B
-  //   let theta1: number = Math.atan(slope1) % (2 * Math.PI);
-  //   if (circ1.y < circ2.y) {
-  //     theta1 += Math.PI;
-  //     theta1 %= Math.PI;
-  //   }
-
-  //   // angle for car to A or B
-  //   let theta2: number = Math.atan(slope2) % (2 * Math.PI);
-  //   // car was to the left of the circle, so function has to add 180 degrees (since arctan(slope) only is for the right side)
-  //   if (car1.pos.x < circ1.x) {
-  //     theta2 += Math.PI; // add 180 degrees
-  //     theta2 %= Math.PI; // wrap around PI
-  //   }
-  //   return r * Math.abs(theta1 - theta2);
-  // }
 }
 
 //console.log('total length: ', getRSR(startCar, goalCar));
