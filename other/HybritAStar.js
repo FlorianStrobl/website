698c29157b9604deadfad8f846a704f93fb62d14
Florian Strobl <FlorianStrobl@outlook.de>
2022-05-08 12:46:31 +0200

    1

--- other/HybritAStar.js
+++ other/HybritAStar.js
@@ -1,34 +1,41 @@
 var HybritAStar;
 (function (HybritAStar) {
-    const exploringResolution = 1;
-    const possibleSteps = ['R', 'r', 'L', 'l', 'S', 's'];
-    const nodes = [];
-    class Node {
-        constructor(prevNode, gCost, hCost, pos, heading) {
-            this.prevNode = prevNode;
-            this.gCost = gCost;
-            this.hCost = hCost;
-            this.pos = pos;
-            this.heading = heading;
-        }
-        getFCost() {
-            return this.gCost + this.hCost;
-        }
-        isOpen() {
-            return this.open;
-        }
-        setOpen(isOpen) {
-            this.open = isOpen;
-        }
+  const exploringResolution = 1;
+  const possibleSteps = ['R', 'r', 'L', 'l', 'S', 's'];
+  const nodes = [];
+  class Node {
+    constructor(prevNode, gCost, hCost, pos, heading) {
+      this.prevNode = prevNode;
+      this.gCost = gCost;
+      this.hCost = hCost;
+      this.pos = pos;
+      this.heading = heading;
     }
-    function main(startCar, endCar, turningRadius) {
-        // get theoretical best paths
-        const rsPath = ReedSheepPaths.getRSR(startCar, endCar, turningRadius);
-        nodes.push(new Node(null, 0, calcHCost(startCar, endCar), startCar.pos, startCar.heading));
-        while (true) {
-        }
+    getFCost() {
+      return this.gCost + this.hCost;
     }
-    function calcHCost(startCar, endCar) {
-        return -1;
+    isOpen() {
+      return this.open;
     }
+    setOpen(isOpen) {
+      this.open = isOpen;
+    }
+  }
+  function main(startCar, endCar, turningRadius) {
+    // get theoretical best paths
+    //const rsPath = ReedSheepPaths.getRSR(startCar, endCar, turningRadius);
+    nodes.push(
+      new Node(
+        null,
+        0,
+        calcHCost(startCar, endCar),
+        startCar.pos,
+        startCar.heading
+      )
+    );
+    while (true) {}
+  }
+  function calcHCost(startCar, endCar) {
+    return -1;
+  }
 })(HybritAStar || (HybritAStar = {}));
